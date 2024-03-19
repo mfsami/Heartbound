a78dc541ee895d3b6b49902af174c418650c90d8
Sami El-sayed <samielsayed39@gmail.com>
2024-03-19 15:58:32 -0600

    Level 2
    Designed level 2

--- Heartbound/Scripts/door.gd
+++ Heartbound/Scripts/door.gd
@@ -2,13 +2,17 @@ extends Area2D
 
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-
+@export var target_level : PackedScene
 
 
 
 func _on_body_entered(body):
 	if body.name == "Guy" and GameManager.points >= GameManager.required1:
 		animated_sprite_2d.animation = "open"
+		
+		if Input.is_action_just_pressed("Enter"):
+			print("1")
+			get_tree().change_scene_to_packed(target_level)
 
 		
 
