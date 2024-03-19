ffe91c7e1e3ef659f8fe0ca8b69a909ebef084a4
Sami El-sayed <samielsayed39@gmail.com>
2024-03-19 16:26:48 -0600

    Added level 1 to level 2 scene change

--- Heartbound/Scripts/door.gd
+++ Heartbound/Scripts/door.gd
@@ -10,12 +10,11 @@ func _on_body_entered(body):
 	if body.name == "Guy" and GameManager.points >= GameManager.required1:
 		animated_sprite_2d.animation = "open"
 		
-		if Input.is_action_just_pressed("Enter"):
-			print("1")
-			get_tree().change_scene_to_packed(target_level)
 
 		
-
+func _input(event):
+	if Input.is_action_just_pressed("Enter") and GameManager.points >= GameManager.required1:
+			get_tree().change_scene_to_packed(target_level)
 
 func _on_body_exited(body):
 	if body.name == "Guy":
