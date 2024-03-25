84119358def458f42a629d611a7ff39014d044fb
Sami El-sayed <samielsayed39@gmail.com>
2024-03-25 17:26:36 -0600

    Made final cutscene
    There's a one frame problem + Needs sound

--- Heartbound-main/Heartbound-main/Heartbound/Scripts/HeartsFinal.gd
+++ Heartbound-main/Heartbound-main/Heartbound/Scripts/HeartsFinal.gd
@@ -3,14 +3,30 @@ extends Node
 @onready var animation_player1 = $Heart1/AnimationPlayer
 @onready var animation_player2 = $Heart2/AnimationPlayer
 @onready var animation_player3 = $Heart3/AnimationPlayer
+@onready var animation_player4 = $BigHeart1/AnimationPlayer
+@onready var animation_player5 = $BigHeart2/AnimationPlayer
+@onready var animation_player6 = $BigHeart3/AnimationPlayer
+@onready var animation_player7 = $BigHeart4/AnimationPlayer
+@onready var animation_player8 = $Restored/AnimationPlayer
+@onready var animation_player9 = $Polygon2D/AnimationPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	animation_player1.play("Up")
 	animation_player2.play("Up2")
 	animation_player3.play("Up3")
+	
 
+	animation_player4.play("Shake1")
+	animation_player5.play("Shake2")
+	animation_player6.play("Shake3")
+	animation_player7.play("Shake4")
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	animation_player8.play("fadeIn")
+	
+	animation_player9.play("ToBlack")
+	
+
