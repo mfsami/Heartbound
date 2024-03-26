ffb1ed747f6653c85c4e811153b21e49652a0872
Sami El-sayed <samielsayed39@gmail.com>
2024-03-25 20:24:13 -0600

    Added item collection sound

--- Heartbound-main/Heartbound-main/Heartbound/Scripts/heart.gd
+++ Heartbound-main/Heartbound-main/Heartbound/Scripts/heart.gd
@@ -3,13 +3,16 @@ extends Area2D
 
 @onready var audio_stream_player = $AudioStreamPlayer
 @onready var animation_player = $AnimationPlayer
+var collected = false
 
 func _ready():
 	animation_player.play("Rise")
 	
 
 func _on_body_entered(body):
-	if body.name == "Guy":
-		queue_free()
+	if body.name == "Guy" and not collected:
+		collected = true
+		visible = false
+		audio_stream_player.play()
 		GameManager.add_points()
  
